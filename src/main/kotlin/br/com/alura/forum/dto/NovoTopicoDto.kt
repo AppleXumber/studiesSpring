f79195461bbfefb8b0260e70717950e7718f3f69
AppleXumber <wayron.jhonny@gmail.com>
2024-10-13 23:28:47 -0300

    Curso Spring Boot 1: capitulo 3, aula 5

---
+++ src/main/kotlin/br/com/alura/forum/dto/NovoTopicoDto.kt
@@ -0,0 +1,6 @@
+package br.com.alura.forum.dto
+
+class NovoTopicoDto(val titulo: String,
+                    val mensagem: String,
+                    val idCurso: Long,
+                    val idAutor: Long)
--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,38 +1,14 @@
 package br.com.alura.forum.service
 
-import br.com.alura.forum.model.Curso
+import br.com.alura.forum.dto.NovoTopicoDto
 import br.com.alura.forum.model.Topico
-import br.com.alura.forum.model.Usuario
 import org.springframework.stereotype.Service
 
 @Service
-class TopicoService(private var topicos: List<Topico>) {
-
-  init{
-    val topico1 = Topico(
-      1,
-      "Duvida Kotlin 1",
-      "Variaveis no Kotlin",
-      curso = Curso(1,"Kotlin","Programação"),
-      autor = Usuario(1,"Ana da Silva","ana@email.com")
-    )
-    val topico2 = Topico(
-      2,
-      "Duvida Kotlin 2",
-      "Variaveis no Kotlin",
-      curso = Curso(1,"Kotlin","Programação"),
-      autor = Usuario(1,"Ana da Silva","ana@email.com")
-    )
-    val topico3 = Topico(
-      3,
-      "Duvida Kotlin 3",
-      "Variaveis no Kotlin",
-      curso = Curso(1,"Kotlin","Programação"),
-      autor = Usuario(1,"Ana da Silva","ana@email.com")
-    )
-
-    topicos = listOf(topico1, topico2, topico3)
-  }
+class TopicoService(private var topicos: List<Topico> = ArrayList(),
+  private val cursoService: CursoService,
+  private val autorService: UsuarioService
+) {
 
   fun listar(): List<Topico> {
     return topicos
@@ -40,6 +16,16 @@ class TopicoService(private var topicos: List<Topico>) {
 
   fun buscarPorId(id: Long): Topico {
     return topicos.stream().filter { t -> t.id == id }.findFirst().get()
+  }
+
+  fun cadastrar(dto: NovoTopicoDto) {
+    topicos.plus(Topico(
+      id = topicos.size.toLong() + 1,
+      titulo = dto.titulo,
+      mensagem = dto.mensagem,
+      curso = cursoService.buscarPorId(dto.idCurso),
+      autor = autorService.buscarPorId(dto.idAutor)
+    ))
   }
 
 }
