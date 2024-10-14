719346eba155c75e59cac4786063090fb7c22624
AppleXumber <wayron.jhonny@gmail.com>
2024-10-14 20:23:42 -0300

    Curso Spring Boot 1: capitulo 3 final

--- src/main/kotlin/br/com/alura/forum/dto/NovoTopicoDto.kt
+++
@@ -1,6 +0,0 @@
-package br.com.alura.forum.dto
-
-class NovoTopicoDto(val titulo: String,
-                    val mensagem: String,
-                    val idCurso: Long,
-                    val idAutor: Long)
