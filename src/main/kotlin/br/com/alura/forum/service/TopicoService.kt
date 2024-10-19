35e405fa45eff473f94841e5a32691f15a1620cd
AppleXumber <wayron.jhonny@gmail.com>
2024-10-19 17:29:13 -0300

    Curso Spring Boot 1: capitulo 4

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,5 +1,6 @@
 package br.com.alura.forum.service
 
+import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.mapper.TopicoFormMapper
@@ -26,10 +27,34 @@ class TopicoService(
     return topicoViewMapper.map(topico)
   }
 
-  fun cadastrar(form: NovoTopicoForm) {
+  fun cadastrar(form: NovoTopicoForm) : TopicoView {
     val topico = topicoFormMapper.map(form)
     topico.id = topicos.size.toLong() + 1
     topicos.add(topico)
+    return topicoViewMapper.map(topico)
+  }
+
+  fun atualizar(form: AtualizacaoTopicoForm) : TopicoView {
+    val topico = topicos.stream().filter { t -> t.id == form.id }.findFirst().get()
+    val index = topicos.indexOfFirst { it.id == form.id }
+    if (index != -1) {
+      topicos[index] = Topico(
+        form.id,
+        form.titulo,
+        form.mensagem,
+        topico.dataCriacao,
+        topico.curso,
+        topico.autor,
+        topico.status,
+        topico.respostas
+      )
+    }
+    return topicoViewMapper.map(topicos[index])
+  }
+
+  fun deletar(id: Long) {
+    val index = topicos.indexOfFirst { it.id == id }
+    topicos.removeAt(index)
   }
 
 }
